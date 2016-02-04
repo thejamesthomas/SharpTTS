21a74c415d7af876d1a8ab1682ac30bea563f4ca
James Thomas <james@duckzilla.com>
2016-02-03 20:14:16 -0600

    [James] Adding a class file I neglected to add earlier

---
+++ SharpTTS/OutputDevice.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using NAudio.Wave;
+
+namespace SharpTTS
+{
+    public class OutputDevice
+    {
+        private readonly WaveOutCapabilities _outputDevice;
+
+        public string Name => _outputDevice.ProductName;
+        public int DeviceNumber { get; }
+
+        public OutputDevice(WaveOutCapabilities outputDevice, int deviceNumber)
+        {
+            _outputDevice = outputDevice;
+            DeviceNumber = deviceNumber;
+        }
+
+        public static List<OutputDevice> GetInstalledOutputDevices()
+        {
+            var outputDevices = new List<OutputDevice>();
+
+            for (var i = 0; i < WaveOut.DeviceCount; i++)
+            {
+                outputDevices.Add(new OutputDevice(WaveOut.GetCapabilities(i), i));
+            }
+
+            return outputDevices;
+        }
+    }
+}
